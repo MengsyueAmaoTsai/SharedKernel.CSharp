d73971e41fa7317e680fd0de695ea72c6b8c95c2
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-02-18 19:23:06 +0800

    SpecificationBuilderTests (OrderBy/OrderByDescending)

---
+++ Tests/RichillCapital.SharedKernel.UnitTests/Specifications/Builders/SpecificationBuilderExtensions.OrderBy.Tests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+
+using RichillCapital.SharedKernel.Specifications.Expressions;
+using RichillCapital.SharedKernel.UnitTests.Specifications.Common.Specs;
+
+namespace RichillCapital.SharedKernel.UnitTests.Specifications.Builders;
+
+public class SpecificationBuilderExtensions_OrderBy
+{
+    [Fact]
+    public void Should_AddNothingToList_When_GivenNoOrderExpression()
+    {
+        // Arrange
+        var spec = new StoreEmptySpec();
+
+        // Act & Assert
+        spec.OrderExpressions.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void Should_AddNothingToList_When_GivenOrderExpressionWithFalseCondition()
+    {
+        // Arrange
+        var spec = new CompanyByIdWithFalseConditions(1);
+
+        // Act & Assert
+        spec.OrderExpressions.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void Should_AddOrderExpressionToListWithOrderByType_When_GivenOrderByExpression()
+    {
+        // Arrange
+        var spec = new StoresOrderedSpecByName();
+
+        // Act & Assert
+        spec.OrderExpressions.Should().ContainSingle();
+        spec.OrderExpressions.Single().OrderType.Should().Be(OrderByExpressionType.OrderBy);
+    }
+}
---
+++ Tests/RichillCapital.SharedKernel.UnitTests/Specifications/Common/Specs/StoresOrderedSpecByName.cs
@@ -0,0 +1,10 @@
+﻿using RichillCapital.SharedKernel.Specifications;
+using RichillCapital.SharedKernel.Specifications.Builders;
+
+namespace RichillCapital.SharedKernel.UnitTests.Specifications.Common.Specs;
+
+public class StoresOrderedSpecByName : Specification<Store>
+{
+    public StoresOrderedSpecByName() =>
+        Query.OrderBy(store => store.Name);
+}
