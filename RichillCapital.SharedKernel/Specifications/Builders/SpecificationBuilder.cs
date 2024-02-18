531edfc4daf76b963345827cef356ca8ce3ca199
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-02-18 20:45:40 +0800

    rafactor

--- RichillCapital.SharedKernel/Specifications/Builders/SpecificationBuilder.cs
+++ RichillCapital.SharedKernel/Specifications/Builders/SpecificationBuilder.cs
@@ -4,20 +4,15 @@ public class SpecificationBuilder<T, TResult> :
     SpecificationBuilder<T>, ISpecificationBuilder<T, TResult>
 {
     public SpecificationBuilder(Specification<T, TResult> specification)
-        : base(specification)
-    {
-        Specification = specification;
-    }
+        : base(specification) => Specification = specification;
 
     public new Specification<T, TResult> Specification { get; }
 }
 
 public class SpecificationBuilder<T> : ISpecificationBuilder<T>
 {
-    public SpecificationBuilder(Specification<T> specification)
-    {
+    public SpecificationBuilder(Specification<T> specification) =>
         Specification = specification;
-    }
 
     public Specification<T> Specification { get; }
 }
