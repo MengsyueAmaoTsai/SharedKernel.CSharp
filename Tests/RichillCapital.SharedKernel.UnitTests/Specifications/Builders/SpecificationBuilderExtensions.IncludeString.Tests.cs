acfe14a6866e98ee39a5e3913a2e9967222d00a3
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-02-18 19:09:55 +0800

    SpecificationBuilderTests (IncludeString/Include)

---
+++ Tests/RichillCapital.SharedKernel.UnitTests/Specifications/Builders/SpecificationBuilderExtensions.IncludeString.Tests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+
+using RichillCapital.SharedKernel.UnitTests.Specifications.Common;
+using RichillCapital.SharedKernel.UnitTests.Specifications.Common.Specs;
+
+namespace RichillCapital.SharedKernel.UnitTests.Specifications.Builders;
+
+public class SpecificationBuilderExtensionsIncludeStringTests
+{
+    [Fact]
+    public void AddsNothingToList_GivenNoIncludeStringExpression()
+    {
+        // Arrange
+        var spec = new StoreEmptySpec();
+
+        // Act & Assert
+        spec.WhereExpressions.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void AddsNothingToList_GivenIncludeStringWithFalseCondition()
+    {
+        // Arrange
+        var spec = new CompanyByIdWithFalseConditions(1);
+
+        // Act & Assert
+        spec.IncludeStrings.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void AddsIncludeStringToList_GivenString()
+    {
+        // Arrange
+        var spec = new StoreIncludeCompanyThenCountryAsStringSpec();
+        var expected = $"{nameof(Company)}.{nameof(Company.Country)}";
+
+        // Act & Assert
+        spec.IncludeStrings.Should().ContainSingle();
+        spec.IncludeStrings.Single().Should().Be(expected);
+    }
+}
