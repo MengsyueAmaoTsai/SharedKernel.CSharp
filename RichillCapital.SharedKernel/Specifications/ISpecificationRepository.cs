df66421565fa819ef09b1c4ac707e6191595afa5
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-02-18 20:27:48 +0800

    Rename DeleteRangeAsync to RemoveRange

--- RichillCapital.SharedKernel/Specifications/ISpecificationRepository.cs
+++ RichillCapital.SharedKernel/Specifications/ISpecificationRepository.cs
@@ -2,5 +2,5 @@ namespace RichillCapital.SharedKernel.Specifications;
 
 public interface ISpecificationRepository<T> : ISpecificationReadOnlyRepository<T>
 {
-    Task DeleteRangeAsync(ISpecification<T> specification, CancellationToken cancellationToken = default);
+    Task RemoveRange(ISpecification<T> specification, CancellationToken cancellationToken = default);
 }
