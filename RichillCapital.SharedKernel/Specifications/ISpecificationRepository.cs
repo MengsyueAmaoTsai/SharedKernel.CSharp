0d4f1e1067090869206e6e372f0bcec54eba2ba0
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-02-18 19:52:05 +0800

    hot fix version - 1.0.7

--- RichillCapital.SharedKernel/Specifications/ISpecificationRepository.cs
+++ RichillCapital.SharedKernel/Specifications/ISpecificationRepository.cs
@@ -1,6 +1,6 @@
 namespace RichillCapital.SharedKernel.Specifications;
 
-public interface ISpecificationRepository<T>
+public interface ISpecificationRepository<T> : ISpecificationReadOnlyRepository<T>
 {
     Task DeleteRangeAsync(ISpecification<T> specification, CancellationToken cancellationToken = default);
 }
