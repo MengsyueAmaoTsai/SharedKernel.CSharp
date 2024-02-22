3297d5bffc953024f72d27ab6705076e51de6ba9
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-02-23 02:56:26 +0800

    add test entity2

--- Tests/RichillCapital.SharedKernel.UnitTests/Common/TestEntity.cs
+++ Tests/RichillCapital.SharedKernel.UnitTests/Common/TestEntity.cs
@@ -2,8 +2,22 @@ namespace RichillCapital.SharedKernel.UnitTests.Common;
 
 internal sealed class TestEntity : Entity<TestEntityId>
 {
-    public TestEntity(TestEntityId id)
+    public TestEntity(TestEntityId id, string name)
         : base(id)
     {
+        Name = name;
     }
+
+    public string Name { get; private set; }
+}
+
+internal sealed class TestEntity2 : Entity<TestEntityId>
+{
+    public TestEntity2(TestEntityId id, string name)
+        : base(id)
+    {
+        Name = name;
+    }
+
+    public string Name { get; private set; }
 }
