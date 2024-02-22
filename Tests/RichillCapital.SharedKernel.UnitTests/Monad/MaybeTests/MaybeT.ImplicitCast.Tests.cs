a77eac650d5ec4d328a5eada7179bd29f8da2bf3
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-02-23 04:42:36 +0800

    Move test cases

--- Tests/RichillCapital.SharedKernel.UnitTests/Monad/MaybeTests/MaybeT.ImplicitCast.Tests.cs
+++ Tests/RichillCapital.SharedKernel.UnitTests/Monad/MaybeTests/MaybeT.ImplicitCast.Tests.cs
@@ -30,63 +30,6 @@ public sealed partial class GenericMaybeTests : MonadTests
     }
 
     [Fact]
-    public void Value_When_MaybeHasValue_Should_ReturnValue()
-    {
-        // Arrange
-        Maybe<int> intMaybe = Maybe<int>.With(IntValue);
-        Maybe<string> stringMaybe = Maybe<string>.With(StringValue);
-        Maybe<bool> boolMaybe = Maybe<bool>.With(BoolValue);
-        Maybe<byte> byteMaybe = Maybe<byte>.With(ByteValue);
-        Maybe<DateTimeOffset> dateTimeMaybe = Maybe<DateTimeOffset>.With(DateTimeValue);
-        Maybe<TestEntity> testEntityMaybe = Maybe<TestEntity>.With(TestEntity);
-
-        // Assert
-        intMaybe.Value.Should().Be(IntValue);
-        stringMaybe.Value.Should().Be(StringValue);
-        boolMaybe.Value.Should().Be(BoolValue);
-        byteMaybe.Value.Should().Be(ByteValue);
-        dateTimeMaybe.Value.Should().Be(DateTimeValue);
-        testEntityMaybe.Value.Should().Be(TestEntity);
-    }
-
-    [Fact]
-    public void Value_When_MaybeHasNoValue_Should_ThrowInvalidOperationException()
-    {
-        // Arrange
-        Maybe<int> intMaybe = Maybe<int>.Null;
-        Maybe<string> stringMaybe = Maybe<string>.Null;
-        Maybe<bool> boolMaybe = Maybe<bool>.Null;
-        Maybe<byte> byteMaybe = Maybe<byte>.Null;
-        Maybe<DateTimeOffset> dateTimeMaybe = Maybe<DateTimeOffset>.Null;
-        Maybe<TestEntity> testEntityMaybe = Maybe<TestEntity>.Null;
-
-        // Act & Assert
-        intMaybe.Invoking(maybe => maybe.Value)
-            .Should().Throw<InvalidOperationException>()
-            .WithMessage($"Maybe<{typeof(int)}> has no value.");
-
-        stringMaybe.Invoking(maybe => maybe.Value)
-            .Should().Throw<InvalidOperationException>()
-            .WithMessage($"Maybe<{typeof(string)}> has no value.");
-
-        boolMaybe.Invoking(maybe => maybe.Value)
-            .Should().Throw<InvalidOperationException>()
-            .WithMessage($"Maybe<{typeof(bool)}> has no value.");
-
-        byteMaybe.Invoking(maybe => maybe.Value)
-            .Should().Throw<InvalidOperationException>()
-            .WithMessage($"Maybe<{typeof(byte)}> has no value.");
-
-        dateTimeMaybe.Invoking(maybe => maybe.Value)
-            .Should().Throw<InvalidOperationException>()
-            .WithMessage($"Maybe<{typeof(DateTimeOffset)}> has no value.");
-
-        testEntityMaybe.Invoking(maybe => maybe.Value)
-            .Should().Throw<InvalidOperationException>()
-            .WithMessage($"Maybe<{typeof(TestEntity)}> has no value.");
-    }
-
-    [Fact]
     public void ImplicitCast_Should_ConvertMaybeToValue()
     {
         // Arrange
