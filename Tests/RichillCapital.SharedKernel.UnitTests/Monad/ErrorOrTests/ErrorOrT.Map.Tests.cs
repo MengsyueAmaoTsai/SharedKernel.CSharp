5f54cc466c6f966c4e2c3c2f4fb9e29ab937fac2
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-02-23 08:38:52 +0800

    ErrorOr Map / Then

--- Tests/RichillCapital.SharedKernel.UnitTests/Monad/ErrorOrTests/ErrorOrT.Map.Tests.cs
+++ Tests/RichillCapital.SharedKernel.UnitTests/Monad/ErrorOrTests/ErrorOrT.Map.Tests.cs
@@ -7,20 +7,26 @@ public sealed partial class GenericErrorOrTests : MonadTests
     [Fact]
     public void Map_WhenIsError_ReturnsErrorOrWithErrors()
     {
+        // Arrange
         var errorOr = ErrorOr<int>.From([TestError]);
 
+        // Act
         var result = errorOr.Map(value => value.ToString());
 
+        // Assert
         result.ShouldBeErrors([TestError]);
     }
 
     [Fact]
     public void Map_WhenIsNotError_ReturnsErrorOrWithValue()
     {
+        // Arrange
         var errorOr = ErrorOr<int>.Is(1);
 
+        // Act
         var result = errorOr.Map(value => value.ToString());
 
+        // Assert
         result.ShouldBeValue("1");
     }
 }
