db4f0051df682c8e9211ae5561fbd5c8f2e18e5a
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-02-24 18:20:37 +0800

    ErrorOr then test

--- RichillCapital.SharedKernel/Monads/ErrorOr.Then.cs
+++ RichillCapital.SharedKernel/Monads/ErrorOr.Then.cs
@@ -2,6 +2,30 @@ namespace RichillCapital.SharedKernel.Monads;
 
 public readonly partial record struct ErrorOr<TValue>
 {
+    public ErrorOr<TValue> Then(Action<TValue> action)
+    {
+        if (IsError)
+        {
+            return ErrorOr<TValue>.From(Errors);
+        }
+
+        action(Value);
+
+        return ErrorOr<TValue>.Is(Value);
+    }
+
+    public ErrorOr<TValue> Then(Action action)
+    {
+        if (IsError)
+        {
+            return ErrorOr<TValue>.From(Errors);
+        }
+
+        action();
+
+        return ErrorOr<TValue>.Is(Value);
+    }
+
     public ErrorOr<TResult> Then<TResult>(Func<TResult> func) =>
         IsError ?
             ErrorOr<TResult>.From(Errors) :
--- Tests/RichillCapital.SharedKernel.UnitTests/Monads/ErrorOrT.Then.Tests.cs
+++ Tests/RichillCapital.SharedKernel.UnitTests/Monads/ErrorOrT.Then.Tests.cs
@@ -7,7 +7,7 @@ namespace RichillCapital.SharedKernel.UnitTests.Monads;
 public sealed partial class GenericErrorOrTests : MonadTests
 {
     [Fact]
-    public void Then_When_ErrorOrIsError_Should_ReturnErrorOrWithErrors()
+    public void Then_When_ErrorOrIsError_ShouldNot_ExecuteFunctionAndReturnErrorOrWithErrors()
     {
         // Arrange & Act
         var errorOrInt = ErrorOr<int>
@@ -23,7 +23,7 @@ public sealed partial class GenericErrorOrTests : MonadTests
     }
 
     [Fact]
-    public void Then_When_ErrorOrIsValue_Should_ReturnErrorOrWithValue()
+    public void Then_When_ErrorOrIsValue_Should_ExecuteFunctionAndReturnErrorOrWithValue()
     {
         // Arrange & Act
         var expectedValue = "Execute then";
@@ -35,5 +35,66 @@ public sealed partial class GenericErrorOrTests : MonadTests
         errorOrInt.IsError.Should().BeFalse();
         errorOrInt.IsValue.Should().BeTrue();
         errorOrInt.Value.Should().Be(expectedValue);
+    }
+
+    [Fact]
+    public void Then_When_ErrorOrIsError_ShouldNot_ExecutionActionAndReturnErrorOrWithErrors()
+    {
+        // Arrange & Act
+        var errorOrInt = ErrorOr<int>
+            .From(NotFoundError)
+            .Then(() => { });
+
+        // Assert
+        errorOrInt.IsError.Should().BeTrue();
+        errorOrInt.IsValue.Should().BeFalse();
+        errorOrInt.Errors
+            .Should().HaveCount(1)
+            .And.BeEquivalentTo(new[] { NotFoundError });
+    }
+
+    [Fact]
+    public void Then_When_ErrorOrIsValue_Should_ExecuteActionAndReturnErrorOrWithValue()
+    {
+        // Arrange & Act
+        var errorOrInt = ErrorOr<int>
+            .Is(IntValue)
+            .Then(() => { });
+
+
+        // Assert
+        errorOrInt.IsError.Should().BeFalse();
+        errorOrInt.IsValue.Should().BeTrue();
+        errorOrInt.Value.Should().Be(IntValue);
+    }
+
+    [Fact]
+    public void Then_When_ErrorOrIsError_ShouldNot_ExecuteActionWithValueAndReturnErrorOrWithErrors()
+    {
+        // Arrange & Act
+        var errorOrInt = ErrorOr<int>
+            .From(NotFoundError)
+            .Then(value => value += 9);
+
+        // Assert
+        errorOrInt.IsError.Should().BeTrue();
+        errorOrInt.IsValue.Should().BeFalse();
+        errorOrInt.Errors
+            .Should().HaveCount(1)
+            .And.BeEquivalentTo(new[] { NotFoundError });
+    }
+
+    [Fact]
+    public void Then_When_ErrorOrIsValue_Should_ExecuteActionWithValueAndReturnErrorOrWithValue()
+    {
+        // Arrange & Act
+        var errorOrInt = ErrorOr<int>
+            .Is(IntValue)
+            .Then(value => value += 9);
+
+        // Assert
+        errorOrInt.IsError.Should().BeFalse();
+        errorOrInt.IsValue.Should().BeTrue();
+        errorOrInt.Value.Should().Be(10);
     }
 }
