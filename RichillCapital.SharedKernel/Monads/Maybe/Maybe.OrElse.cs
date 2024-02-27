3c2d91855ee3963bd419d021b1f1f15cbc098b63
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-02-28 05:17:13 +0800

    Else method Maybe / Result

--- RichillCapital.SharedKernel/Monads/Maybe/Maybe.OrElse.cs
+++
@@ -1,9 +0,0 @@
-namespace RichillCapital.SharedKernel.Monads;
-
-public readonly partial record struct Maybe<TValue>
-{
-    public Maybe<TValue> OrElse(TValue value) =>
-        HasNoValue ?
-            Maybe<TValue>.Have(value) :
-            Maybe<TValue>.Have(_value);
-}
