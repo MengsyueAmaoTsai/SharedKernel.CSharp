c6e40ce6077ac181d2ebfaf5cf882a588381199a
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-02-28 19:34:11 +0800

    Throw if white space

--- RichillCapital.SharedKernel/Diagnostics/ThrowableExtensions.String.cs
+++ RichillCapital.SharedKernel/Diagnostics/ThrowableExtensions.String.cs
@@ -2,17 +2,23 @@ namespace RichillCapital.SharedKernel.Diagnostics;
 
 public static partial class ThrowableExtensions
 {
-    public static Throwable<string> IfEmpty(this Throwable<string> throwable)
-    {
-        return throwable;
-    }
-
     public static ref readonly Throwable<string> IfWhiteSpace(
         this in Throwable<string> throwable)
     {
         if (throwable.Value.All(char.IsWhiteSpace))
         {
             Thrower.Throw(throwable.ParamName, "String should not be white space only.");
+        }
+
+        return ref throwable;
+    }
+
+    public static ref readonly Throwable<string> IfEmpty(
+        this in Throwable<string> throwable)
+    {
+        if (throwable.Value.Length == 0)
+        {
+            Thrower.Throw(throwable.ParamName, "String should not be empty.");
         }
 
         return ref throwable;
--- Tests/RichillCapital.SharedKernel.UnitTests/Diagnostics/ThrowableExtensions.String.Tests.cs
+++ Tests/RichillCapital.SharedKernel.UnitTests/Diagnostics/ThrowableExtensions.String.Tests.cs
@@ -7,7 +7,7 @@ namespace RichillCapital.SharedKernel.UnitTests.Diagnostics;
 public sealed partial class ThrowableExtensionsTests
 {
     [Fact]
-    public void Throw_IfWhiteSpace_When_WhiteSpace_Should_Throw_ArgumentException()
+    public void Throw_IfWhiteSpace_When_WhiteSpace_Should_ThrowArgumentException()
     {
         // Arrange
         string value = " ";
@@ -27,6 +27,32 @@ public sealed partial class ThrowableExtensionsTests
 
         // Act
         Action action = () => value.Throw().IfWhiteSpace();
+
+        // Assert
+        action.Should().NotThrow();
+    }
+
+    [Fact]
+    public void Throw_IfEmpty_When_Empty_Should_ThrowArgumentException()
+    {
+        // Arrange
+        string value = string.Empty;
+        Action action = () => value.Throw().IfEmpty();
+
+        // Assert
+        action.Should()
+            .ThrowExactly<ArgumentException>()
+            .WithMessage($"String should not be empty. (Parameter '{nameof(value)}')");
+    }
+
+    [Fact]
+    public void Throw_IfEmpty_When_NotEmpty_Should_NotThrow()
+    {
+        // Arrange
+        string value = "a";
+
+        // Act
+        Action action = () => value.Throw().IfEmpty();
 
         // Assert
         action.Should().NotThrow();
