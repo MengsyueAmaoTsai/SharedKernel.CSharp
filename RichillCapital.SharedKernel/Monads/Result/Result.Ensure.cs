547852e52afcf9a35df5caafdcf29afed1e8ba61
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-02-29 13:27:12 +0800

    Ensure factory method tests

--- RichillCapital.SharedKernel/Monads/Result/Result.Ensure.cs
+++ RichillCapital.SharedKernel/Monads/Result/Result.Ensure.cs
@@ -2,14 +2,17 @@ namespace RichillCapital.SharedKernel.Monads;
 
 public readonly partial record struct Result<TValue>
 {
-    public static Result<TValue> Ensure(TValue value, Func<TValue, bool> ensure, Error error) =>
+    public static Result<TValue> Ensure(
+        TValue value,
+        Func<TValue, bool> ensure,
+        Error error) =>
         !ensure(value) ?
             error.ToResult<TValue>() :
             value.ToResult();
 
     public static Result<TValue> Ensure(
         TValue value,
-        (Func<TValue, bool> ensure, Error error)[] rules)
+        params (Func<TValue, bool> ensure, Error error)[] rules)
     {
         var results = rules
             .Select(rule => Ensure(value, rule.ensure, rule.error))
