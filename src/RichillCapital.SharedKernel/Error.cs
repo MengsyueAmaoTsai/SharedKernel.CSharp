8ea8cbd53a9d82b885d0273eae9f075dc95c5cbe
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2025-01-10 20:41:41 +0800

    Add public static method Create for creating Error type

--- src/RichillCapital.SharedKernel/Error.cs
+++ src/RichillCapital.SharedKernel/Error.cs
@@ -13,19 +13,19 @@ public readonly record struct Error
 
     public string Message { get; private init; }
 
-    public static Error Invalid(string code, string message) => new(ErrorType.Validation, code, message);
-    public static Error Unauthorized(string code, string message) => new(ErrorType.Unauthorized, code, message);
-    public static Error Forbidden(string code, string message) => new(ErrorType.Forbidden, code, message);
-    public static Error NotFound(string code, string message) => new(ErrorType.NotFound, code, message);
-    public static Error Conflict(string code, string message) => new(ErrorType.Conflict, code, message);
-    public static Error Unexpected(string code, string message) => new(ErrorType.Unexpected, code, message);
-    public static Error Unavailable(string code, string message) => new(ErrorType.Unavailable, code, message);
-
-    public static Error Invalid(string message) => new(ErrorType.Validation, nameof(ErrorType.Validation), message);
-    public static Error Unauthorized(string message) => new(ErrorType.Unauthorized, nameof(ErrorType.Unauthorized), message);
-    public static Error Forbidden(string message) => new(ErrorType.Forbidden, nameof(ErrorType.Forbidden), message);
-    public static Error NotFound(string message) => new(ErrorType.NotFound, nameof(ErrorType.NotFound), message);
-    public static Error Conflict(string message) => new(ErrorType.Conflict, nameof(ErrorType.Conflict), message);
-    public static Error Unexpected(string message) => new(ErrorType.Unexpected, nameof(ErrorType.Unexpected), message);
-    public static Error Unavailable(string message) => new(ErrorType.Unavailable, nameof(ErrorType.Unavailable), message);
+    public static Error Create(ErrorType type, string code, string message) => new(type, code, message);
+    public static Error Invalid(string code, string message) => Create(ErrorType.Validation, code, message);
+    public static Error Unauthorized(string code, string message) => Create(ErrorType.Unauthorized, code, message);
+    public static Error Forbidden(string code, string message) => Create(ErrorType.Forbidden, code, message);
+    public static Error NotFound(string code, string message) => Create(ErrorType.NotFound, code, message);
+    public static Error Conflict(string code, string message) => Create(ErrorType.Conflict, code, message);
+    public static Error Unexpected(string code, string message) => Create(ErrorType.Unexpected, code, message);
+    public static Error Unavailable(string code, string message) => Create(ErrorType.Unavailable, code, message);
+    public static Error Invalid(string message) => Create(ErrorType.Validation, nameof(ErrorType.Validation), message);
+    public static Error Unauthorized(string message) => Create(ErrorType.Unauthorized, nameof(ErrorType.Unauthorized), message);
+    public static Error Forbidden(string message) => Create(ErrorType.Forbidden, nameof(ErrorType.Forbidden), message);
+    public static Error NotFound(string message) => Create(ErrorType.NotFound, nameof(ErrorType.NotFound), message);
+    public static Error Conflict(string message) => Create(ErrorType.Conflict, nameof(ErrorType.Conflict), message);
+    public static Error Unexpected(string message) => Create(ErrorType.Unexpected, nameof(ErrorType.Unexpected), message);
+    public static Error Unavailable(string message) => Create(ErrorType.Unavailable, nameof(ErrorType.Unavailable), message);
 }
