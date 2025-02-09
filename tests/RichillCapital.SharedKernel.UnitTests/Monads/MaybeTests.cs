c652dccc59a581cf9ee35beec34d42379429bfd5
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2025-02-09 19:18:17 +0800

    Add Maybe tests

--- tests/RichillCapital.SharedKernel.UnitTests/Monads/MaybeTests.cs
+++ tests/RichillCapital.SharedKernel.UnitTests/Monads/MaybeTests.cs
@@ -24,4 +24,48 @@ public sealed class MaybeTests
         maybe.IsNull.ShouldBeFalse();
         maybe.Value.ShouldBe(stringValue);
     }
+
+    [Fact]
+    public void Null_Should_ReturnNullMaybe()
+    {
+        Maybe<string> maybe = Maybe<string>.Null();
+
+        maybe.IsNull.ShouldBeTrue();
+        maybe.HasValue.ShouldBeFalse();
+        Should.Throw<InvalidOperationException>(
+            () => maybe.Value,
+            Maybe<string>.AccessValueOnNullMaybeMessage);
+    }
+
+    [Fact]
+    public void Maybes_WithSameValue_Should_BeEqual()
+    {
+        var sameValue = "value";
+
+        (Maybe<string> maybe1, Maybe<string> maybe2) = (
+            Maybe<string>.With(sameValue),
+            Maybe<string>.With(sameValue));
+
+        maybe1.ShouldBe(maybe2);
+    }
+
+    [Fact]
+    public void Maybes_WithDifferentValues_ShouldNotBeEqual()
+    {
+        (Maybe<string> maybe1, Maybe<string> maybe2) = (
+            Maybe<string>.With("value1"),
+            Maybe<string>.With("value2"));
+
+        maybe1.ShouldNotBe(maybe2);
+    }
+
+    [Fact]
+    public void NullMaybes_Should_BeEqual()
+    {
+        (Maybe<string> maybe1, Maybe<string> maybe2) = (
+            Maybe<string>.Null(),
+            Maybe<string>.Null());
+
+        maybe1.ShouldBe(maybe2);
+    }
 }
