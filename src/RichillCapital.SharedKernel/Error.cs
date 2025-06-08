5f898b7506fac215e7169a51c148ac210b776ad6
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2025-06-08 15:15:45 +0800

    add ErrorType.cs

--- src/RichillCapital.SharedKernel/Error.cs
+++ src/RichillCapital.SharedKernel/Error.cs
@@ -207,21 +207,3 @@ public readonly record struct Error
     /// <returns>The created timeout error.</returns>
     public static Error Timeout(string message) => Create(ErrorType.Timeout, nameof(ErrorType.Timeout), message);
 }
-
-/// <summary>
-/// Represents the types of errors that can occur.
-/// </summary>
-public enum ErrorType
-{
-    Null = 0,
-    Validation = 400,
-    Unauthorized = 401,
-    AccessDenied = 403,
-    NotFound = 404,
-    MethodNotAllowed = 405,
-    Conflict = 409,
-    UnsupportedMediaType = 415,
-    Unexpected = 500,
-    Unavailable = 503,
-    Timeout = 504,
-}
